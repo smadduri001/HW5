b78426f40f2187bf749f3bc79c79e20982d76e13
Sri Siddhartha Madduri <smadduri@ucsd.edu>
2023-03-02 19:32:25 -0800

    custom

--- customdialog.js
+++ customdialog.js
@@ -11,4 +11,46 @@ function init() {
         })
     });
 
+    document.getElementById("confirmButton").addEventListener("click", function() {
+        outputBox.innerHTML = ``;
+        setTimeout(() => {
+            let confirmDialog = document.getElementById("confirmDialog");
+            confirmDialog.showModal();
+            document.getElementById("confirmCancelButton").addEventListener("click", function() {
+                confirmDialog.close();
+                outputBox.innerHTML = `The value returned by the confirm method is : false`;
+            });
+            document.getElementById("confirmOkButton").addEventListener("click", function() {
+                confirmDialog.close();
+                outputBox.innerHTML = `The value returned by the confirm method is : true`;
+            });
+        }, 0);
+    });
+
+    document.getElementById("promptButton").addEventListener("click", function() {
+        outputBox.innerHTML = ``;
+        setTimeout(() => {
+            let promptDialog = document.getElementById("promptDialog");
+            promptDialog.showModal();
+            document.getElementById("promptCancelButton").addEventListener("click", function() {
+                confirmDialog.close();
+                outputBox.innerHTML = `User didn't enter anything`;
+            });
+            document.getElementById("promptOkButton").addEventListener("click", function() {
+                let userInput = document.getElementById("promptInput");
+                if(userInput.value == null) {
+                    confirmDialog.close();
+                    outputBox.innerHTML = `User didn't enter anything`;
+                }
+                else {
+                    setTimeout(() => {
+                        let purified = DOMPurify.sanitize(userInput.value);
+                        confirmDialog.close();
+                        outputBox.innerHTML = `Prompt result: ${purified}`;
+                    }, 0);
+                }
+            });
+        }, 0);
+    });
+
 }
